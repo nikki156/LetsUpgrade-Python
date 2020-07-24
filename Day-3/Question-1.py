a49a663d0a17e51fb43cc3fa11de986270ee8203
Nikita Chavan <46427331+nikkicodes@users.noreply.github.com>
2020-07-24 23:37:24 +0530

    Assignment3

---
+++ Day-3/Question-1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+sum=0
+n=int(input("Enter a value:"))
+while n>0:
+  sum=sum+n
+  n=n-1
+print("Sum of numbers is",sum)
+
+
+# In[ ]:
+
+
+
+
